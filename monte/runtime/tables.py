113e2f0e62b53f651a05b384a63eb3d5219e98e6
Corbin Simpson <MostAwesomeDude@gmail.com>
2014-02-25 02:10:42 -0800

    monte/runtime/tables: Add FlexList and ConstList.diverge().

--- monte/runtime/tables.py
+++ monte/runtime/tables.py
@@ -1,3 +1,6 @@
+from monte.runtime.base import MonteObject
+
+
 class ConstList(tuple):
     def __repr__(self):
         orig = tuple.__repr__(self)
@@ -10,6 +13,25 @@ class ConstList(tuple):
     #XXX Is this a good name/API? no idea.
     def contains(self, item):
         return item in self
+
+    def diverge(self):
+        return FlexList(list(tuple(self)))
+
+
+class FlexList(MonteObject):
+
+    def __init__(self, l):
+        self.l = l
+
+    def push(self, value):
+        self.l.append(value)
+
+    def pop(self):
+        return self.l.pop()
+
+    def readOnly(self):
+        return ConstList(self.l)
+
 
 def makeMonteList(*items):
     return ConstList(items)
