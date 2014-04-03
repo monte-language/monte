ab658d2765189d6cb691c7235a0908df4a43d24d
Corbin Simpson <MostAwesomeDude@gmail.com>
2014-04-02 21:37:15 -0700

    runtime/trace: Make a small effort to encode Unicode to UTF-8.

--- monte/runtime/trace.py
+++ monte/runtime/trace.py
@@ -1,5 +1,9 @@
 def trace(x):
+    if isinstance(x, unicode):
+        x = x.encode("utf-8")
     print x,
 
 def traceln(x):
+    if isinstance(x, unicode):
+        x = x.encode("utf-8")
     print x
