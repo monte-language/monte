e8d4eefa25f82369ef10c476d8ffd6715df5e694
Mike Cooper <mythmon@gmail.com>
2014-04-17 10:27:25 -0700

    Fix string slicing, add a test.

--- monte/runtime/data.py
+++ monte/runtime/data.py
@@ -499,8 +499,11 @@ class String(MonteObject):
     def slice(self, start, end=None):
         if not isinstance(start, Integer):
             raise RuntimeError("%r is not an integer" % (start,))
+        start = start.n
         if end is not None and not isinstance(end, Integer):
             raise RuntimeError("%r is not an integer" % (end,))
+        elif end is not None:
+            end = end.n
         if start < 0:
             raise RuntimeError("Slice indices must be positive")
         if end is not None and end < 0:
