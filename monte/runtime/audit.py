752a5077e4dba43aa2dd28917249beec785c1f91
Allen Short <ashort@mozilla.com>
2014-11-09 20:25:37 -0800

    audit caching

--- monte/runtime/audit.py
+++ monte/runtime/audit.py
@@ -7,7 +7,7 @@ from monte.runtime.guards.data import booleanGuard
 class Audition(MonteObject):
     _m_fqn = "Audition"
 
-    def __init__(self, fqn, expr, bindings, obj, outerNames):
+    def __init__(self, fqn, expr, bindings, obj, outerNames, cache):
         self.expr = expr
         self.bindings = bindings
         self.approvers = []
@@ -15,14 +15,48 @@ class Audition(MonteObject):
         self.fqn = fqn
         self._active = True
         self.outerNames = outerNames
+        self.askedLog = []
+        self.guardLog = []
+        self.auditorCache = cache
+
 
     def ask(self, auditor):
         if not self._active:
             raise RuntimeError("audition is out of scope")
-        #XXX caching of audit results
-        result = booleanGuard.coerce(auditor.audit(self), throw)
-        if result is true:
-            self.approvers.append(auditor)
+        doCaching = deepFrozenGuard in auditor._m_auditorStamps
+        cached = False
+        if doCaching:
+            if id(auditor) in self.auditorCache:
+                answer, asked, guards = self.auditorCache[id(auditor)]
+
+                for name, value in guards:
+                    if not (self.bindings.get(typecheck(name, Twine).bare().s) == value):
+                        break
+                else:
+                    cached = True
+
+        if cached:
+            for a in asked:
+                self.ask(a)
+            if answer is true:
+                self.approvers.append(auditor)
+            return answer
+        else:
+            prevlogs = self.askedLog, self.guardLog
+            self.askedLog = []
+            self.guardLog = []
+            try:
+                #print "%s auditing %s" % (auditor, self.fqn)
+                result = booleanGuard.coerce(auditor.audit(self), throw)
+                if doCaching and self.guardLog is not None:
+                    #print "audit cached:", result
+                    self.auditorCache[id(auditor)] = (result, self.askedLog[:], self.guardLog[:])
+                if result is true:
+                    #print self.fqn, "approved by", auditor
+                    self.approvers.append(auditor)
+            finally:
+                self.askedLog, self.guardLog = prevlogs
+            return result
 
     def getObjectExpr(self):
         return self.expr
@@ -30,9 +64,16 @@ class Audition(MonteObject):
     def getGuard(self, name):
         n = typecheck(name, Twine).bare().s
         if n not in self.bindings:
+            self.guardLog = None
             raise RuntimeError('"%s" is not a free variable in %s' %
                                (name, str(self.obj)))
-        return self.bindings[n]
+        answer = self.bindings[n]
+        if self.guardLog is not None:
+            if deepFrozenGuard in answer._m_auditorStamps:
+                self.guardLog.append((name, answer))
+            else:
+                self.guardLog = None
+        return answer
 
     def getFQN(self):
         return self.fqn
