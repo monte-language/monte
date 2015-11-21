b597ad6ff8da745e937a864c9551472b2f3ec702
Dan Connolly <dckc@madmode.com>
2015-11-21 03:11:38 -0600

    regenerate MonteSyntax.hs

--- docs/MonteSyntax.hs
+++ docs/MonteSyntax.hs
@@ -1,16 +1,37 @@
 
 {-
-DefExpr ::= Sequence(Sigil('def', NonTerminal('pattern')),
-         Optional(Sigil("exit", NonTerminal('order'))),
+guardOpt ::= Maybe(Sigil(':',
+ Choice(
+     0,
+     Ap('GetExpr',
+        Ap('NounExpr', 'IDENTIFIER'),
+        Brackets('[', SepBy(NonTerminal('expr'), ','), ']')),
+     Ap('NounExpr', 'IDENTIFIER'),
+     Brackets('(', NonTerminal('expr'), ')'))))
+-}
+guardOpt = optionMaybe guardOpt_1
+  where
+    guardOpt_1 = ((symbol ":") *> guardOpt_1_2)
+    guardOpt_1_2 = guardOpt_1_2_1
+      <|> guardOpt_1_2_2
+      <|> guardOpt_1_2_3
+    guardOpt_1_2_1 = GetExpr <$> guardOpt_1_2_1_1 <*> guardOpt_1_2_1_2
+    guardOpt_1_2_1_1 = NounExpr <$> parseIdentifier
+    guardOpt_1_2_1_2 = ((bra "[") *> guardOpt_1_2_1_2_2 <* (ket "]"))
+    guardOpt_1_2_1_2_2 = (sepBy expr (symbol ","))
+    guardOpt_1_2_2 = NounExpr <$> parseIdentifier
+    guardOpt_1_2_3 = ((bra "(") *> expr <* (ket ")"))
+
+{-
+DefExpr ::= Ap('DefExpr', Sigil('def', NonTerminal('pattern')),
+         Maybe(Sigil("exit", NonTerminal('order'))),
          Sigil(":=", NonTerminal('assign')))
 -}
 defExpr = DefExpr <$> defExpr_1 <*> defExpr_2 <*> defExpr_3
   where
     defExpr_1 = ((symbol "def") *> pattern)
-    defExpr_2 = defExpr_2_1
-      <|> defExpr_2_2
-    defExpr_2_1 = 
-    defExpr_2_2 = ((symbol "exit") *> order)
+    defExpr_2 = optionMaybe defExpr_2_1
+    defExpr_2_1 = ((symbol "exit") *> order)
     defExpr_3 = ((symbol ":=") *> assign)
 
 {-
@@ -27,35 +48,35 @@ order = binaryExpr
 
 {-
 BinaryExpr ::= Choice(0,
-  Sequence(NonTerminal('prefix'),
+  Ap('BinaryExpr', NonTerminal('prefix'),
            "**", NonTerminal('order')),
-  Sequence(NonTerminal('prefix'),
+  Ap('BinaryExpr', NonTerminal('prefix'),
            Choice(0, "*", "/", "//", "%"), NonTerminal('order')),
-  Sequence(NonTerminal('prefix'),
+  Ap('BinaryExpr', NonTerminal('prefix'),
            Choice(0, "+", "-"), NonTerminal('order')),
-  Sequence(NonTerminal('prefix'),
+  Ap('BinaryExpr', NonTerminal('prefix'),
            Choice(0, "<<", ">>"), NonTerminal('order')))
 -}
-binaryExpr = BinaryExpr <$> binaryExpr_1
-  <|> BinaryExpr <$> binaryExpr_2
-  <|> BinaryExpr <$> binaryExpr_3
-  <|> BinaryExpr <$> binaryExpr_4
+binaryExpr = binaryExpr_1
+  <|> binaryExpr_2
+  <|> binaryExpr_3
+  <|> binaryExpr_4
   where
-    binaryExpr_1 = prefix (symbol "**") order
-    binaryExpr_2 = prefix binaryExpr_2_2 order
+    binaryExpr_1 = BinaryExpr <$> prefix <*> (symbol "**") <*> order
+    binaryExpr_2 = BinaryExpr <$> prefix <*> binaryExpr_2_2 <*> order
     binaryExpr_2_2 = (symbol "*")
       <|> (symbol "/")
       <|> (symbol "//")
       <|> (symbol "%")
-    binaryExpr_3 = prefix binaryExpr_3_2 order
+    binaryExpr_3 = BinaryExpr <$> prefix <*> binaryExpr_3_2 <*> order
     binaryExpr_3_2 = (symbol "+")
       <|> (symbol "-")
-    binaryExpr_4 = prefix binaryExpr_4_2 order
+    binaryExpr_4 = BinaryExpr <$> prefix <*> binaryExpr_4_2 <*> order
     binaryExpr_4_2 = (symbol "<<")
       <|> (symbol ">>")
 
 {-
-CompareExpr ::= Sequence(NonTerminal('prefix'),
+CompareExpr ::= Ap('CompareExpr', NonTerminal('prefix'),
          Choice(0, ">", "<", ">=", "<=", "<=>"), NonTerminal('order'))
 -}
 compareExpr = CompareExpr <$> prefix <*> compareExpr_2 <*> order
@@ -67,7 +88,7 @@ compareExpr = CompareExpr <$> prefix <*> compareExpr_2 <*> order
       <|> (symbol "<=>")
 
 {-
-RangeExpr ::= Sequence(NonTerminal('prefix'),
+RangeExpr ::= Ap('RangeExpr', NonTerminal('prefix'),
          Choice(0, "..", "..!"), NonTerminal('order'))
 -}
 rangeExpr = RangeExpr <$> prefix <*> rangeExpr_2 <*> order
@@ -98,40 +119,28 @@ prim = literalExpr
   <|> mapExpr
   <|> listExpr
   where
-    prim_4 = ((symbol "(") *> expr <* (symbol ")"))
+    prim_4 = ((bra "(") *> expr <* (ket ")"))
 
 {-
-HideExpr ::= Brackets("{", SepBy(NonTerminal('expr'), ';', fun='wrapSequence'), "}")
+HideExpr ::= Ap('HideExpr',
+   Brackets("{", SepBy(NonTerminal('expr'), ';', fun='wrapSequence'), "}"))
 -}
-hideExpr = HideExpr <$> ((symbol "{") *> hideExpr_2 <* (symbol "}"))
+hideExpr = HideExpr <$> hideExpr_1
   where
-    hideExpr_2 = (wrapSequence expr (symbol ";"))
+    hideExpr_1 = ((bra "{") *> hideExpr_1_2 <* (ket "}"))
+    hideExpr_1_2 = (wrapSequence expr (symbol ";"))
 
 {-
-NounExpr ::= Choice(0, "IDENTIFIER", Sigil("::", ".String."))
+NounExpr ::= Ap('NounExpr', Choice(0, "IDENTIFIER", Sigil("::", ".String.")))
 -}
-nounExpr = NounExpr <$> parseIdentifier
-  <|> NounExpr <$> nounExpr_2
+nounExpr = NounExpr <$> nounExpr_1
   where
-    nounExpr_2 = ((symbol "::") *> parseString)
+    nounExpr_1 = parseIdentifier
+      <|> nounExpr_1_2
+    nounExpr_1_2 = ((symbol "::") *> parseString)
 
 {-
-prefix ::= Choice(
- 0,
- NonTerminal('unary'),
- NonTerminal('SlotExpression'),
- NonTerminal('BindingExpression'),
- Sequence(NonTerminal('call'), NonTerminal('guardOpt')))
--}
-prefix = unary
-  <|> slotExpression
-  <|> bindingExpression
-  <|> prefix_4
-  where
-    prefix_4 = call guardOpt
-
-{-
-FinalPatt ::= Sequence(Choice(0, "IDENTIFIER", Sigil("::", ".String.")),
+FinalPatt ::= Ap('FinalPatt', Choice(0, "IDENTIFIER", Sigil("::", ".String.")),
          NonTerminal('guardOpt'))
 -}
 finalPatt = FinalPatt <$> finalPatt_1 <*> guardOpt
@@ -141,42 +150,74 @@ finalPatt = FinalPatt <$> finalPatt_1 <*> guardOpt
     finalPatt_1_2 = ((symbol "::") *> parseString)
 
 {-
-IntExpr ::= Sequence(Terminal(".int."))
+quasiliteral ::= Ap('QuasiParserExpr',
+ Maybe(Terminal("IDENTIFIER")),
+ Brackets('`',
+ SepBy(
+     Choice(0,
+       Ap('Left', Terminal('QUASI_TEXT')),
+       Ap('Right',
+         Choice(0,
+           Ap('NounExpr', Terminal('DOLLAR_IDENT')),
+           Brackets('${', NonTerminal('expr'), '}'))))),
+ '`'))
+-}
+quasiliteral = QuasiParserExpr <$> quasiliteral_1 <*> quasiliteral_2
+  where
+    quasiliteral_1 = optionMaybe parseIdentifier
+    quasiliteral_2 = ((bra "`") *> quasiliteral_2_2 <* (ket "`"))
+    quasiliteral_2_2 = (many0 quasiliteral_2_2_1_1)
+    quasiliteral_2_2_1_1 = quasiliteral_2_2_1_1_1
+      <|> quasiliteral_2_2_1_1_2
+    quasiliteral_2_2_1_1_1 = Left <$> parseQuasiText
+    quasiliteral_2_2_1_1_2 = Right <$> quasiliteral_2_2_1_1_2_1
+    quasiliteral_2_2_1_1_2_1 = quasiliteral_2_2_1_1_2_1_1
+      <|> quasiliteral_2_2_1_1_2_1_2
+    quasiliteral_2_2_1_1_2_1_1 = NounExpr <$> parseDollarIdent
+    quasiliteral_2_2_1_1_2_1_2 = ((bra "${") *> expr <* (ket "}"))
+
+{-
+IntExpr ::= Ap('IntExpr', Terminal(".int."))
 -}
 intExpr = IntExpr <$> parseint
 
 {-
-DoubleExpr ::= Sequence(Terminal(".float64."))
+DoubleExpr ::= Ap('DoubleExpr', Terminal(".float64."))
 -}
 doubleExpr = DoubleExpr <$> parsefloat64
 
 {-
-CharExpr ::= Sequence(Terminal(".char."))
+CharExpr ::= Ap('CharExpr', Terminal(".char."))
 -}
 charExpr = CharExpr <$> parseChar
 
 {-
-StrExpr ::= Sequence(Terminal(".String."))
+StrExpr ::= Ap('StrExpr', Terminal(".String."))
 -}
 strExpr = StrExpr <$> parseString
 
 {-
-ListExpr ::= Brackets("[", SepBy(NonTerminal('expr'), ','), "]")
+ListExpr ::= Ap('ListExpr', Brackets("[", SepBy(NonTerminal('expr'), ','), "]"))
 -}
-listExpr = ListExpr <$> ((symbol "[") *> listExpr_2 <* (symbol "]"))
+listExpr = ListExpr <$> listExpr_1
   where
-    listExpr_2 = (sepBy expr (symbol ","))
+    listExpr_1 = ((bra "[") *> listExpr_1_2 <* (ket "]"))
+    listExpr_1_2 = (sepBy expr (symbol ","))
 
 {-
-MapExpr ::= Brackets("[",
-         SepBy(Pair(NonTerminal('expr'), "=>", NonTerminal('expr')),
-               ','),
-         "]")
+MapExpr ::= Ap('MapExpr',
+  Brackets("[",
+           SepBy(Ap('pair', NonTerminal('expr'),
+                            Sigil("=>", NonTerminal('expr'))),
+                 ','),
+           "]"))
 -}
-mapExpr = MapExpr <$> ((symbol "[") *> mapExpr_2 <* (symbol "]"))
+mapExpr = MapExpr <$> mapExpr_1
   where
-    mapExpr_2 = (sepBy mapExpr_2_2_1 (symbol ","))
-    mapExpr_2_2_1 = pair <$> expr <*> ((symbol "=>") *> expr)
+    mapExpr_1 = ((bra "[") *> mapExpr_1_2 <* (ket "]"))
+    mapExpr_1_2 = (sepBy mapExpr_1_2_1_1 (symbol ","))
+    mapExpr_1_2_1_1 = pair <$> expr <*> mapExpr_1_2_1_1_2
+    mapExpr_1_2_1_1_2 = ((symbol "=>") *> expr)
 
 {-
 LiteralExpr ::= Choice(0,
